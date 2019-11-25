057f83a3ccc52485a2f9d50d4e23e58968fffcde
memsharded <james@conan.io>
2019-11-25 23:35:30 +0100

    conanfile.txt simple consumer

--- image-filter/deploy.sh
+++
@@ -1,7 +0,0 @@
-conan create . conan/stable --profile ./armv8
-rm -rf build
-mkdir -p build
-cd build
-conan install image-filter/0.0.1@conan/stable --profile ../armv8
-scp bin/* root@192.168.10.100:/bin
-cd ..
